a41eddcc022cb4fae85c6a6e39fb2daeb3498707
stojanov-d <stojanovdavid3@gmail.com>
2024-08-30 21:56:56 +0200

    feat: Add Authentication using JWT Bearer tokens and cookies

--- BE-membership-connect/Program.cs
+++ BE-membership-connect/Program.cs
@@ -3,6 +3,8 @@ using BE_membership_connect.Models;
 using dotenv.net;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,7 +39,7 @@ if (builder.Environment.IsStaging())
       options.UseNpgsql("DefaultConnection"));
 }
 
-builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+builder.Services.AddIdentityCore<AppUser>(options =>
 {
   options.User.RequireUniqueEmail = true;
   options.Password.RequireDigit = true;
@@ -47,15 +49,51 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
   options.Password.RequiredLength = 8;
 })
   .AddEntityFrameworkStores<AppDbContext>()
-  .AddDefaultTokenProviders();
+  .AddApiEndpoints();
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = IdentityConstants.BearerScheme;
+  options.DefaultChallengeScheme = IdentityConstants.BearerScheme;
+})
+    .AddBearerToken(IdentityConstants.BearerScheme)
+    .AddCookie(IdentityConstants.ApplicationScheme);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "Membership Connect API", Version = "v1" });
 
+  // Define the Bearer scheme used for authentication
+  c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+  {
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    Scheme = "bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "Enter your token in the text input below. Example: '12345abcdef'."
+  });
+
+  c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -68,6 +106,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.MapIdentityApi<AppUser>();
 
 app.MapControllers();
 
